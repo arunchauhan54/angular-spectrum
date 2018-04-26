76be738adc6f1b269d2841755dd82fad9a304cc2
Arun <arunchauhan54@gmail.com>
2018-04-26 22:43:28 +1000

    created Angular sample Layout

--- angular-view-encapsulation/src/app/app.module.ts
+++ angular-view-encapsulation/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {MaterialModule} from "./material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatAutocompleteModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+import { OrderItemComponent } from './order-item/order-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OrderItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
